50096d95d8ead50d285f65c068086b83311f8088
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-04-18 09:04:46 -0500

    Config.env

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config({path: '.env'})
 const db = new Sequelize(process.env.BD_NOMBRE,process.env.BD_USER,process.env.BD_PASS ?? '',{
   host: process.env.BD_HOST,
-  port: 5432,
-  dialect: 'postgres',
+  port: process.env.BD_PORT,
+  dialect: process.env.BD_DIALECT,
   define: {
     timestamps: true
   },
