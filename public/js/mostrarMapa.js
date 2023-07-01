4df2bddd160718a02d6d1947c24ee772e284de43
unknown <arcadio.2es0@gmail.com>
2023-06-30 18:05:23 -0600

    FixLoadJs-1.0

--- public/js/mostrarMapa.js
+++ public/js/mostrarMapa.js
@@ -16,7 +16,7 @@
   \*******************************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n(function() {\n  const lat = document.querySelector('#lat').textContent\n  const lng = document.querySelector('#lng').textContent\n  const titulo = document.querySelector('#titulo').textContent\n  const mapa = L.map('mapa').setView([lat,lng],16)\n\n  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\n    attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\n}).addTo(mapa);\n\n  // Agregar el Pin\n  L.marker([lat,lng])\n    .addTo(mapa)\n    .bindPopup(titulo)\n})()\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/mostrarMapa.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n(function() {\r\n  const lat = document.querySelector('#lat').textContent\r\n  const lng = document.querySelector('#lng').textContent\r\n  const titulo = document.querySelector('#titulo').textContent\r\n  const mapa = L.map('mapa-prop').setView([lat,lng],16)\r\n\r\n  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {\r\n    attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'\r\n}).addTo(mapa);\r\n\r\n  // Agregar el Pin\r\n  L.marker([lat,lng])\r\n    .addTo(mapa)\r\n    .bindPopup(titulo)\r\n})()\n\n//# sourceURL=webpack://bienesraices_mvc/./src/js/mostrarMapa.js?");
 
 /***/ })
 
--- src/js/mostrarMapa.js
+++ src/js/mostrarMapa.js
@@ -2,7 +2,7 @@
   const lat = document.querySelector('#lat').textContent
   const lng = document.querySelector('#lng').textContent
   const titulo = document.querySelector('#titulo').textContent
-  const mapa = L.map('mapa').setView([lat,lng],16)
+  const mapa = L.map('mapa-prop').setView([lat,lng],16)
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
