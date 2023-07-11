b7be6cc1fd6c15085ea624cbf1de1d02e4a9eed7
unknown <arcadio.2es0@gmail.com>
2023-07-10 18:13:06 -0600

    AllCoordenadasEstados

--- seed/estados.js
+++ seed/estados.js
@@ -7,188 +7,188 @@ const estados = [
   },
   {
     nombre: 'Baja California',
-    lat: 5,
-    lng: 8,
+    lat: 32.63576001517278,
+    lng: -115.46877996785044,
     zoom: 10
   },
   {
     nombre: 'Baja California Sur',
-    lat: 5,
-    lng: 8,
+    lat: 24.140488665878856,
+    lng: -110.31232016913178,
     zoom: 10
   },
   {
     nombre: 'Campeche',
-    lat: 5,
-    lng: 8,
+    lat: 19.840359163296824,
+    lng: -90.51358670152207,
     zoom: 10
   },
   {
     nombre: 'Chiapas',
-    lat: 5,
-    lng: 8,
+    lat: 16.753660000000025,
+    lng: -93.11661999999995,
     zoom: 10
   },
   {
     nombre: 'Chihuahua',
-    lat: 5,
-    lng: 8,
+    lat: 28.660410004044138,
+    lng: -106.09355999440587,
     zoom: 10
   },
   {
     nombre: 'Ciudad de México',
-    lat: 5,
-    lng: 8,
+    lat: 19.431017817707705,
+    lng: -99.13340960961861,
     zoom: 10
   },
   {
     nombre: 'Coahuila',
-    lat: 5,
-    lng: 8,
+    lat: 25.419730000000072,
+    lng: -100.99993999999998,
     zoom: 10
   },
   {
     nombre: 'Colima',
-    lat: 5,
-    lng: 8,
+    lat: 19.24473815833129,
+    lng: -103.72945443522009,
     zoom: 10
   },
   {
     nombre: 'Durango',
-    lat: 5,
-    lng: 8,
+    lat: 24.01889000740782,
+    lng: -104.65572002560633,
     zoom: 10
   },
   {
     nombre: 'Estado de México',
-    lat: 5,
-    lng: 8,
+    lat: 19.289919567768266,
+    lng: -99.65850070108192,
     zoom: 10
   },
   {
     nombre: 'Guanajuato',
-    lat: 5,
-    lng: 8,
+    lat: 21.014350000000036,
+    lng: -101.25389999999999,
     zoom: 10
   },
   {
     nombre: 'Guerrero',
-    lat: 5,
-    lng: 8,
+    lat: 17.553989988544018,
+    lng: -99.50482997088918,
     zoom: 10
   },
   {
     nombre: 'Hidalgo',
-    lat: 5,
-    lng: 8,
+    lat: 20.11637001769448,
+    lng: -98.74216001422245,
     zoom: 10
   },
   {
     nombre: 'Jalisco',
-    lat: 5,
-    lng: 8,
+    lat: 20.666070000000047,
+    lng: -103.34017999999998,
     zoom: 10
   },
   {
     nombre: 'Michoacán',
-    lat: 5,
-    lng: 8,
+    lat: 19.70225000000005,
+    lng: -101.19597999999996,
     zoom: 10
   },
   {
     nombre: 'Morelos',
-    lat: 5,
-    lng: 8,
+    lat: 18.918840009215685,
+    lng: -99.23461995983246,
     zoom: 10
   },
   {
     nombre: 'Nayarit',
-    lat: 5,
-    lng: 8,
+    lat: 21.50858000000005,
+    lng: -104.89264999999995,
     zoom: 10
   },
   {
     nombre: 'Nuevo León',
-    lat: 5,
-    lng: 8,
+    lat: 25.673800020326766,
+    lng: -100.3194699903575,
     zoom: 10
   },
   {
     nombre: 'Oaxaca',
-    lat: 5,
-    lng: 8,
+    lat: 17.059406091737387,
+    lng: -96.73182059584963,
     zoom: 10
   },
   {
     nombre: 'Puebla',
-    lat: 5,
-    lng: 8,
+    lat: 19.033450004194606,
+    lng: -98.20380998691721,
     zoom: 10
   },
   {
     nombre: 'Querétaro',
-    lat: 5,
-    lng: 8,
+    lat: 20.587889999424988,
+    lng: -100.39877000283393,
     zoom: 10
   },
   {
     nombre: 'Quintana Roo',
-    lat: 5,
-    lng: 8,
+    lat: 18.510799993998745,
+    lng: -88.30139001286653,
     zoom: 10
   },
   {
     nombre: 'San Luis Potosí',
-    lat: 5,
-    lng: 8,
+    lat: 22.14659000000006,
+    lng: -100.97393999999997,
     zoom: 10
   },
   {
     nombre: 'Sinaloa',
-    lat: 5,
-    lng: 8,
+    lat: 24.80376592212491,
+    lng: -107.39341825250989,
     zoom: 10
   },
   {
     nombre: 'Sonora',
-    lat: 5,
-    lng: 8,
+    lat: 29.09202001480945,
+    lng: -110.97071997400873,
     zoom: 10
   },
   {
     nombre: 'Tabasco',
-    lat: 5,
-    lng: 8,
+    lat: 17.987120015540683,
+    lng: -92.93850997134042,
     zoom: 10
   },
   {
     nombre: 'Tamaulipas',
-    lat: 5,
-    lng: 8,
+    lat: 23.73633999085814,
+    lng: -99.14328998846155,
     zoom: 10
   },
   {
     nombre: 'Tlaxcala',
-    lat: 5,
-    lng: 8,
+    lat: 19.31651000000005,
+    lng: -98.23874999999998,
     zoom: 10
   },
   {
     nombre: 'Veracruz',
-    lat: 5,
-    lng: 8,
+    lat: 19.539009984367993,
+    lng: -96.9155600000711,
     zoom: 10
   },
   {
     nombre: 'Yucatán',
-    lat: 5,
-    lng: 8,
+    lat: 20.963990000000024,
+    lng: -89.62928999999997,
     zoom: 10
   },
   {
     nombre: 'Zacatecas',
-    lat: 5,
-    lng: 8,
+    lat: 22.760979995485854,
+    lng: -102.5903699652247,
     zoom: 10
   },
 ]
