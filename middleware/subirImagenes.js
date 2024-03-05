b4b03c4eb448507dc17d54c461de928b39d9459c
unknown <arcadio.2es0@gmail.com>
2024-03-05 05:52:45 -0600

    UploadIMGS

--- middleware/subirImagenes.js
+++ middleware/subirImagenes.js
@@ -4,7 +4,7 @@ import { generarId } from '../helpers/tokens.js';
 
 const storage = multer.diskStorage({
   destination: function(req,file,cb){
-    cb(null, './public/uploads/profiles/');
+    cb(null, './public/uploads/');
   },
   filename: function(req,file,cb){
     cb(null, generarId() + path.extname(file.originalname));
